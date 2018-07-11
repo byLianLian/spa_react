f2c7871c9fc034db0403b987f34375da4870e55b
byLianLian <bylianlian@163.com>
2018-07-11 14:47:09 +0800

    commit spa

---
+++ spa/src/component/nav/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import {Link} from 'react-router-dom';
+
+const Nav = props => (
+	<ul className="nav">
+		{
+			props.list.map((ele, idx) => (
+				<li key={idx}><Link to={ele.url}>{ele.text}</Link></li>
+			))
+		}
+	</ul>
+)
+
+export default Nav;
+
---
+++ spa/src/reducer/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+import {goods} from './goods.js'
+
+export const rootReducer = combineReducers({
+	goods
+})
